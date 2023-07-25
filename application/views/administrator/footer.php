408ce14b683f9e82e3772aed4ef29c6801843444
Rizki Agung Sentosa <77620549+rizkiagungid@users.noreply.github.com>
2023-07-25 12:26:30 +0700

    Update footer.php

--- application/views/administrator/footer.php
+++ application/views/administrator/footer.php
@@ -1,2 +1,3 @@
-
-<strong>Copyright &copy; 2016 - <?php echo date('Y'); ?> <a target='_BLANK' href="http://www.lokomedia.web.id"> Website Sekolah Lokomedia</a>.</strong> All rights reserved. 
+<strong>Copyright &copy; 2022 -
+    <?php echo date('Y'); ?> <a target='_BLANK' href="http://dev.rasxmedia.com/u/rasx"> RizkiAgungId</a>.
+</strong> All rights reserved.
