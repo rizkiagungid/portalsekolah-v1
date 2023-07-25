cd58c913000394e2bc52c5dba85ea20b4aaf3adb
Rizki Agung Sentosa <77620549+rizkiagungid@users.noreply.github.com>
2023-07-25 12:25:05 +0700

    Update footer.php

--- application/views/lokomedia_sekolah/footer.php
+++ application/views/lokomedia_sekolah/footer.php
@@ -1,7 +1,4 @@
-
-<?php 
+<?php
 echo "<div class='wrapper' style='padding-top:0px;'><br>
-	<center>Lokomedia web Sekolah © ".date('Y')." All Right Reserved</center>
+	<center>RizkiAgungid © " . date('Y') . " All Right Reserved</center>
 </div>";
-
-
